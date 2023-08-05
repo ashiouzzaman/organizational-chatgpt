10002546a84287b5f1e71a7be8e90fbbb3742f14
ashiouzzaman <ashiouzzaman.code@gmail.com>
2023-08-05 19:10:40 +0600

    Knowledge base

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 def main():
     load_dotenv()
 
-    # Base interface using streamlit
+    # base interface using streamlit
     st.set_page_config(page_title="ChatGPT project by Real")
     st.header("Organizational ChatGPT 💬")
 
     # Uploading data
     pdf = st.file_uploader("Upload your data: ", type="pdf")
 
-    #Extract data
+    # extract data
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
         text = ""
@@ -21,7 +23,7 @@ def main():
             text+= page.extract_text()
         # st.write(text)
 
-        #Split into chunks
+        # split into chunks
         text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
@@ -31,6 +33,12 @@ def main():
 
         chunks = text_splitter.split_text(text)
         st.write(chunks)
+
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+
 
 if __name__ == '__main__':
     main()
